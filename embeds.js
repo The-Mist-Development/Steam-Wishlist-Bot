96e3b82ad208c43f2348655b26204868ef3f30a5
R2D2VaderBeef <52345992+R2D2VaderBeef@users.noreply.github.com>
2022-05-15 11:16:40 +0100

    Bot can use wrapper to get game info

--- embeds.js
+++ embeds.js
@@ -13,7 +13,7 @@ module.exports = {
                 },
                 {
                     name: `\`${prefix}register\``,
-                    value: "Starts the process (which continues in DM) of acquiring your Steam wishlist, so we can notify you when games on it go on sale."
+                    value: "Starts the process (which continues in DMs) of acquiring your Steam wishlist, so we can notify you when games on it go on sale."
                 },
                 {
                     name: `\`${prefix}list\``,
@@ -35,6 +35,26 @@ module.exports = {
                     name: `\`${prefix}game\``,
                     value: "Looks up a Steam game by ID and provides information about it. This was initially intended for debugging."
                 }
+            )
+    },
+    privacyembed: function() {
+        return new MessageEmbed()
+            .setTitle("Privacy")
+            .setColor("#10b555")
+            .setDescription("The original source code for the bot can be found [here](https://github.com/The-Mist-Development/Steam-Wishlist-Bot). \r Steam Wishlist Bot is not affiliated with Valve or Steam.")
+            .addFields(
+                {
+                    name: "Data we store",
+                    value: "When you register, we store part of your Steam profile link and your Discord ID in our database. This allows us to update which games we should notify you about when your wishlist changes. \r We also store Steam's Game ID for all of the games in your wishlist, along with your Discord ID. This allows us to notify you when we detect a sale."
+                },
+                {
+                    name: "How we access data from Steam",
+                    value: "Steam has a public API which can be accessed without an API key. We can get Wishlist data from any profile with Game Details set to public. Also, we can see information about any game, including the price and whether it's on sale."
+                },
+                {
+                    name: "How we can tell when information changes",
+                    value: "We don't know immediately when you have changed your wishlist or a game goes on sale. This is because we update our data at a set interval - every 24 hours for wishlists (unless you trigger a manual resync) and every hour for game prices."
+                } 
             )
     }
 }
--- index.js
+++ index.js
@@ -3,19 +3,19 @@
 require("dotenv").config();
 const wishlist = require("./src/manager");
 
-// The rest of this file is just the bad Discord bot I designed to use the code.
-const {Client, Intents, MessageEmbed} = require("discord.js");
-const { helpembed } = require("./embeds");
+// The rest of this file and other JS files outside the /src directory are just the bad Discord bot I designed to use the code.
+const { Client, Intents, MessageEmbed } = require("discord.js");
+const { helpembed, privacyembed } = require("./embeds");
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES]});
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES] });
 client.login(process.env.TOKEN);
 
 client.on("ready", () => {
     console.log("Bot online!");
-    client.user.setActivity("$help and your wishlists!", {type: "WATCHING"});
+    client.user.setActivity("$help and your wishlists!", { type: "WATCHING" });
 });
 
-client.on("messageCreate", async (message) => { 
+client.on("messageCreate", async (message) => {
     if (message.author.bot) return;
     if (!message.content.startsWith("$")) return;
     let args = message.content.slice(1).split(" ");
@@ -23,9 +23,63 @@ client.on("messageCreate", async (message) => {
 
     switch (command) {
         case "help":
-            message.channel.send({embeds: [helpembed("$")]});
+            message.channel.send({ embeds: [helpembed("$")] });
+            break;
+        case "privacy":
+            message.channel.send({ embeds: [privacyembed("$")] });
+            break;
+        case "game":
+            if (args[0]) sendGameInfo(message, args[0]);
+            else message.channel.send("Please provide a Steam Game ID.");
             break;
         default:
             message.channel.send(`\`${command}\` is not a command. Type \`$help\` to view the list of commands.`);
     }
 });
+
+function sendGameInfo(message, id) {
+    wishlist.getSteamGame(id).then(function (response) {
+        let embed = new MessageEmbed()
+            .setTitle(response.name)
+            .setDescription(response.short_description)
+            .setImage(response.header_image)
+            .setColor("#de66d0")
+        if (response.price_overview) {
+            embed.addFields(
+                {
+                    name: `Price: ${response.price_overview.final_formatted}`,
+                    value: response.price_overview.discount_percent > 0 ? `Discount: ${response.price_overview.discount_percent}%` : "Currently no discount."
+                },
+
+            )
+        }
+        else if (response.is_free){
+            embed.addFields(
+                {
+                    name: "Price: Free",
+                    value: "You can't get more discounted than that!"
+                }
+            )
+        }
+        else {
+            embed.addFields(
+                {
+                    name: "Price: Unknown",
+                    value: "This game may not be available for purchase."
+                }
+            )
+        }
+        message.channel.send({ embeds: [embed] });
+    }).catch(function (error) {
+        if (error == "GAME_NOT_FOUND") {
+            message.channel.send("**Game not found**. Please ensure the Steam ID is correct.");
+        }
+        else if (error == "INVALID_GAME_ID") {
+            message.channel.send("Please provide a valid Steam Game ID.")
+        }
+        else {
+            console.log("Error getting game from Steam: " + error);
+            message.channel.send("An error occured! Try again later.");
+        }
+    });
+}
